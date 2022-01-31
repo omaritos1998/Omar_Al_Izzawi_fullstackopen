6d32a88ce55c1783ac859b8a5e05df70690be7bc
omaritos1998 <omaritos1998gmail.comgit config --global user.email omaritos1998gmail.com>
2022-02-01 00:57:13 +0200

    Part B - Done

---
+++ Part1/B/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Header = (props) => {
+  console.log(props.course);
+  
+  return (
+    <h1>
+     {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+
+  const parts = props.parts;
+  const contentList = parts.map((part) =>
+    <li key={part.exercises}>{part.name} {part.exercises}</li> 
+  );
+
+  return (
+    <ul>
+      {contentList}
+    </ul>
+  )
+}
+
+const Total = (props) => {
+  const calculate = props.parts.reduce((x,y) =>  x = x + y.exercises , 0 )
+  return (
+    <p>
+     Number of exercises {calculate}
+    </p>
+  )
+}
+
+const App = () => {
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+
+return (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+}
+
+export default App
